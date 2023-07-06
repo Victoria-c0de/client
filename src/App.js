cb3444511f033efca3eafa7b041ae44fa1dd461f
Victoria Molina C <victoria.developer023@gmail.com>
2023-07-06 04:04:20 -0400

    Crud en proyecto react ,configuraciones de axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Axios from "axios";
 
 function App() {
+  const [nombre, setNombre] = useState("");
+  const [edad, setEdad] = useState(0);
+  const [pais, setPais] = useState("");
+  const [cargo, setCargo] = useState("");
+  const [anios, setAnios] = useState(0);
+
+  const add = () => {
+    Axios.post("http://localhost:3001/create", {
+      nombre: nombre,
+      edad: edad,
+      pais: pais,
+      cargo: cargo,
+      anios: anios,
+    }).then(() => {
+      alert("Empleado registrado");
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="datos">
+        <label>
+          Nombre:{" "}
+          <input
+            onChange={(event) => {
+              setNombre(event.target.value);
+            }}
+            type="text"
+          />
+        </label>
+        <label>
+          Edad:{" "}
+          <input
+            onChange={(event) => {
+              setEdad(event.target.value);
+            }}
+            type="number"
+          />
+        </label>
+        <label>
+          Pais:{" "}
+          <input
+            onChange={(event) => {
+              setPais(event.target.value);
+            }}
+            type="text"
+          />
+        </label>
+        <label>
+          Cargo:{" "}
+          <input
+            onChange={(event) => {
+              setCargo(event.target.value);
+            }}
+            type="text"
+          />
+        </label>
+        <label>
+          Años:{" "}
+          <input
+            onChange={(event) => {
+              setAnios(event.target.value);
+            }}
+            type="number"
+          />
+        </label>
+        <button onClick={add}>Registrar</button>
+      </div>
     </div>
   );
 }
